317a246a83bf1445e2a5d49077f6bfbb5f17360a
Pandavboy <pandavboy.idec@gmail.comgit config --listcat ~/.gitconfiggit config --listgit config --global user.name Pandavboygit config --global user.email pandavboy.idec@gmail.com>
2021-10-03 10:40:31 +0300

    Исправление для критерия B11

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 // Первая задача. Проверка случайного числа.
-const checkRandomNumber = (a, b) => {
-  if (a >= b ) {
-    return ('Неверное число. Введите число больше 0.')
+const checkRandomNumber = (num1, num2) => {
+  if (num1 >= num2 ) {
+    return ('Неверное число. Введите число больше 0.');
   }
-  return Math.floor(Math.random(a) * b);
+  return Math.floor(Math.random(num1) * num2);
 
 };
 
-console.log(checkRandomNumber(0, 10));
+checkRandomNumber(0, 10);
 
 // Вторая задача. Проверка длины строки.
 const checkString = (str, number) => {
   if (str.length >= number) {
-    return ('Вы превысили допустимое число знаков')
+    return ('Вы превысили допустимое число знаков');
   }
-  return ('Все правильно')
-}
+  return ('Все правильно');
+};
 
-console.log(checkString('улыбка', 5));
+checkString('улыбка', 5);
