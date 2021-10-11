4d0ec19b5bc4f4458df3003c08d6153e5ad7f022
Pandavboy <pandavboy.idec@gmail.comgit config --listcat ~/.gitconfiggit config --listgit config --global user.name Pandavboygit config --global user.email pandavboy.idec@gmail.com>
2021-10-11 23:10:30 +0300

    Создал массив из объектов и добавил ключ id

--- js/main.js
+++ js/main.js
@@ -18,3 +18,37 @@ const checkString = (str, number) => {
 };
 
 checkString('улыбка', 5);
+
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const description = [
+  'Мы в музее',
+  'Гуляем с собачкой',
+  'Вечерний Киев',
+  'Снова в школу',
+];
+
+const names = ['Артем', 'Виктор', 'Александр', 'Олег'];
+function randomObject() {
+  return {
+    url: `photos/${checkRandomNumber(1, 25)}.jpg`,
+    description: description[checkRandomNumber(0, description.length)],
+    likes: checkRandomNumber(15, 200),
+    comments: messages[checkRandomNumber(0, names.length)],
+    names: names[checkRandomNumber(0, names.length)],
+    avatar: `img/avatar-${checkRandomNumber(0, 6)}.svg`,
+  };
+}
+
+
+const similarArray = Array.from({length: 25}, randomObject);
+const similarWithId = similarArray.map((item, index) => ({
+  ...item,
+  id: index + 1,
+}));
